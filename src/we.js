bee4d230262a60ca1805ddd04cc73e517a0b358a
Harshini Vijendra Kumar <109748977+chinni-03@users.noreply.github.com>
2024-05-19 13:20:35 +0530

    Update we.js

--- src/we.js
+++ src/we.js
@@ -1,24 +1,30 @@
 const exp = [
     {
         id: 1,
+        title: "Web Development Intern",
+        company: "Zidio Development",
+        desc: "Currently collaborating on MERN stack projects, ensuring data security, and optimizing user experience. Participating in training to master web dev technologies and best practices."
+    },
+    {
+        id: 2,
         title: "Android Developer Intern",
         company: "CodeClause",
         desc: "Interned in Android development, using Java for backend"
     },
     {
-        id: 2,
+        id: 3,
         title: "Software Development Intern",
         company: "Prodigy InfoTech",
         desc: "Contributed to the development of MERN stack websites as part of an internship program"
     },
     {
-        id: 3,
+        id: 4,
         title: "Web Development and Design Intern",
         company: "Oasis Infobyte",
         desc: "Played a key role in developing websites utilizing HTML5, CSS, EJS, NodeJS, and MongoDB"
     },
     {
-        id: 4,
+        id: 5,
         title: "Python Development Intern",
         company: "CodSoft",
         desc: "Developed Python GUI applications using Tkinter"

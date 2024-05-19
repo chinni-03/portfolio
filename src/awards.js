2d60a44204af6ddc6e2e776118f39196b974dbc5
Harshini Vijendra Kumar <109748977+chinni-03@users.noreply.github.com>
2024-05-19 13:16:31 +0530

    Update awards.js

--- src/awards.js
+++ src/awards.js
@@ -1,9 +1,21 @@
 const awards = [
     {
         id: 1,
-        title: "2nd place, IEEE Enigma Codathon 2023",
+        title: "2nd place, IEEE Enigma Codathon, Oct 2023",
         institute: "Presidency University",
         desc: "Recognized for collaborative development of an Android application"
+    },
+    {
+        id: 2,
+        title: "3rd place, Poetry Competition",
+        institute: "All India Association on Montfort Schools",
+        desc: "Awarded the 3rd place for poetry writing in senior category"
+    },
+    {
+        id: 3,
+        title: "2nd place, Poetry Competition, Feb 2020",
+        institute: "Vagdevi Vilas School",
+        desc: "Won the 2nd place for poetry writing in senior category"
     }
 ]
 
